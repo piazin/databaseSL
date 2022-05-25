205cf006d8be837c17e558f18d2ebcec3f504f60
Lucas <ls4803326@gmail.com>
2022-05-25 17:26:24 -0300

    .env add

--- database/database.js
+++ database/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const connection = new Sequelize('docsl','root','Piazin25$',{
+const connection = new Sequelize('docsl','root',process.env.PASS_BD,{
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-03:00'
